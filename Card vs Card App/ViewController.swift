462653b30a2c64f359293f00159874edfc67aedc
Jackson Taylor <closprieur@gmail.com>
2016-09-04 11:03:25 +1000

    Add randomiser to display cards

--- Card vs Card App/ViewController.swift
+++ Card vs Card App/ViewController.swift
@@ -33,7 +33,23 @@ class ViewController: UIViewController {
 
     @IBAction func playRoundTapped(sender: UIButton) {
     
+        // Randomize a number for the first image view
+        let firstRandomNumber = arc4random_uniform(13) + 1
         
+        // Construct a string with the random number
+        let firstCardString: String = String(format: "card%i", firstRandomNumber)
+        
+        // Set the first card image view to the asset corresponding to the random number
+        self.firstCardImageView.image = UIImage(named: firstCardString)
+        
+        // Randomize a number for the second image view
+        let secondRandomNumber = arc4random_uniform(13) + 1
+        
+        // Construct a string with the random number
+        let secondCardString: String = String(format: "card%i", secondRandomNumber)
+        
+        // Set the second card image view to the asset corresponding to the random number
+        self.secondCardImageView.image = UIImage(named: secondCardString)
     
     }
 
